86dbdcb7f4051e02f63b811abe030cf42630b75e
SamuelChauche <samuel.chauche@gmail.com>
2025-01-30 09:34:35 +0100

    Initialize

---
+++ morpion/lib/views/index.rb
@@ -0,0 +1,11 @@
+class Player
+  
+  def initialize
+  puts "Quel est le prénom du joueur 1 ?"
+  print ">"
+  user_name1 = gets.chomp
+  puts "Quel est le prénom du joueur 1 ?"
+  print ">"
+  user_name2 = gets.chomp
+  end
+end
